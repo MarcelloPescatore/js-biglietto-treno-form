bb05b0ce9889c025ce1cfe9230feac5cfedf2d7c
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-10-09 15:23:05 +0200

    complete milestone 1

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,73 @@
 // MILESTONE 1: 
 // Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
 
+// set up 
 
+// select the form
+const formEl = document.querySelector('form')
+// select the number of kilometers
+const numberKmInputEl = document.getElementById('numberKm')
+// select the traveler age
+const numberAgeInputEl = document.getElementById('travelerAge')
+
+// print in console the variables
+console.log(formEl, numberAgeInputEl, numberKmInputEl);
+
+
+// Create an event listner
+formEl.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const travelerAge = e.target.travelerAge.value;
+    const numberKm = e.target.numberKm.value;
+
+    console.log(numberKm, travelerAge);
+
+    if (numberKm > 0 && travelerAge > 0) {
+
+        // stamp the km in console
+        const yourKm = `Km: ${numberKm}`;
+        console.log(yourKm);
+        
+        // stamp the age in console
+        const yourAge = `Età: ${travelerAge}`;
+        console.log(yourAge);
+        
+        // Now set up the rules to find out the final price
+        
+        // First rule: find the ticket price
+        let costKm = (numberKm * 0.21).toFixed(2) ;
+        // stamp the final price without discount
+        console.log(`Prezzo totale: ${costKm}€`);
+        
+        // Conditions for access to discount
+        // Second rule: if the user age is minor than 18, apply a discount to costKm
+        if (travelerAge < 18) {
+            const discountAmount = (costKm * 20 /100).toFixed(2);
+            const priceDiscounted = costKm - discountAmount;
+            // stamp discount
+            console.log(`Sconto applicato: -${discountAmount}€`);
+            // fix the cost in 2 decimal
+            const finalPrice = priceDiscounted.toFixed(2);
+            // stamp the final price discounted
+            console.log(`Prezzo totale scontato: ${finalPrice}€`);
+        } 
+        // Third rule: if the user age is higher than 65, apply a discount to costKm
+        else if (travelerAge > 65){
+            const discountAmount = (costKm * 40 /100).toFixed(2);
+            const priceDiscounted = costKm - discountAmount;
+            // stamp discount
+            console.log(`Sconto applicato: -${discountAmount}€`);
+            // fix the cost in 2 decimal
+            const finalPrice = priceDiscounted.toFixed(2);
+            // stamp the final price discounted
+            console.log(`Prezzo totale scontato: ${finalPrice}€`);
+        } 
+    
+    } else{
+        console.log('Dati non validi');
+    }
+    
+})
 
 
 
